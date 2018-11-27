08892075025ff5d6a51316b1ad8b73bf1aa63179
Maxim <legckostupov.maxim@yandex.ru>
2018-11-27 22:19:07 +0300

    ver. 0.11

--- game.js
+++ game.js
@@ -1,1 +1,36 @@
 'use strict';
+class Vector {
+  constructor(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
+  }
+
+  plus(x2, y2) {
+    let oldVector = this;
+    let newVector = new Vector(x2, y2);
+    newVector.x = newVector.x + oldVector.x;
+    newVector.y = newVector.y + oldVector.y;
+    return newVector;
+  }
+  times(n) {
+    let newVector = new Vector(this.x, this.y);
+    newVector.x = this.x * n;
+    newVector.y = this.y * n;
+    return newVector;
+  }
+}
+
+class Actor {
+
+}
+
+class Level {
+
+}
+
+const grid = [
+  new Array(3),
+  ['wall', 'wall', 'lava']
+];
+const level = new Level(grid);
+runLevel(level, DOMDisplay);
